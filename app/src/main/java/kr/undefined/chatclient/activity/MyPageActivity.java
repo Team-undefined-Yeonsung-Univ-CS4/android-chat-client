42684ffbdb543abeb29424798c126b66e8994d62
DESKTOP-UV21EQH\zx221 <zx2210@naver.com>
2024-06-10 22:18:37 +0900

    프로필, 배너, 닉네임, 상태메시지 realtimedatabase에 저장되도록 변경

--- app/src/main/java/kr/undefined/chatclient/activity/MyPageActivity.java
+++ app/src/main/java/kr/undefined/chatclient/activity/MyPageActivity.java
@@ -24,10 +24,13 @@ import androidx.constraintlayout.widget.ConstraintLayout;
 import com.bumptech.glide.Glide;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
+import com.google.firebase.database.ValueEventListener;
 import com.google.firebase.storage.FirebaseStorage;
 import com.google.firebase.storage.StorageReference;
 import com.google.firebase.storage.UploadTask;
@@ -49,6 +52,8 @@ public class MyPageActivity extends AppCompatActivity {
     private ImageView ivProfileImg;
     private ImageView ivBannerImg;
     private String currentImageType;
+    private EditText etNickName;
+    private EditText etStatusMsg;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -61,8 +66,8 @@ public class MyPageActivity extends AppCompatActivity {
         ConstraintLayout rootLayout = findViewById(R.id.root_layout);
         ivBannerImg = findViewById(R.id.iv_banner_img);
         ivProfileImg = findViewById(R.id.iv_profile_img);
-        EditText etNickName = findViewById(R.id.et_nickname);
-        EditText etStatusMsg = findViewById(R.id.et_status_msg);
+        etNickName = findViewById(R.id.et_nickname);
+        etStatusMsg = findViewById(R.id.et_status_msg);
         FrameLayout btnEditProfile = findViewById(R.id.btn_edit_profile);
         FrameLayout btnEditBanner = findViewById(R.id.btn_edit_banner);
         ImageButton btnAliasWizard = findViewById(R.id.btn_alias_wizard);
@@ -84,9 +89,96 @@ public class MyPageActivity extends AppCompatActivity {
             currentImageType = "profile";
             selectImage();
         });
-        btnSave.setOnClickListener(view -> uploadImages());
+        btnSave.setOnClickListener(view -> {
+            uploadImages();
+            updateUserData();
+
+        });
         btnSignOut.setOnClickListener(view -> signOut());
         loadImages();
+        loadUserData();
+    }
+
+    private void updateUserData() {
+        String newNickName = etNickName.getText().toString();
+        String newStatusMsg = etStatusMsg.getText().toString();
+
+        Intent intent = getIntent();
+        if (intent != null) {
+            String userId = intent.getStringExtra("UID");
+            if (userId != null) {
+                DatabaseReference userRef = FirebaseDatabase.getInstance().getReference("users").child(userId);
+
+                userRef.addListenerForSingleValueEvent(new ValueEventListener() {
+                    @Override
+                    public void onDataChange(@NonNull DataSnapshot snapshot) {
+                        boolean needsUpdate = false;
+
+                        String currentName = snapshot.child("name").getValue(String.class);
+                        if (!newNickName.isEmpty() && (currentName == null || !currentName.equals(newNickName))) {
+                            needsUpdate = true;
+                            userRef.child("name").setValue(newNickName).addOnCompleteListener(task -> {
+                                if (task.isSuccessful()) {
+                                    Toast.makeText(MyPageActivity.this, "이름 업데이트 성공", Toast.LENGTH_SHORT).show();
+                                } else {
+                                    Toast.makeText(MyPageActivity.this, "이름 업데이트 실패", Toast.LENGTH_SHORT).show();
+                                }
+                            });
+                        }
+
+                        String currentStatusMsg = snapshot.child("statusMsg").getValue(String.class);
+                        if (!newStatusMsg.isEmpty() && (currentStatusMsg == null || !currentStatusMsg.equals(newStatusMsg))) {
+                            needsUpdate = true;
+                            userRef.child("statusMsg").setValue(newStatusMsg).addOnCompleteListener(task -> {
+                                if (task.isSuccessful()) {
+                                    Toast.makeText(MyPageActivity.this, "상태 메시지 업데이트 성공", Toast.LENGTH_SHORT).show();
+                                } else {
+                                    Toast.makeText(MyPageActivity.this, "상태 메시지 업데이트 실패", Toast.LENGTH_SHORT).show();
+                                }
+                            });
+                        }
+
+                        if (!needsUpdate) {
+                            Toast.makeText(MyPageActivity.this, "변경된 내용이 없습니다.", Toast.LENGTH_SHORT).show();
+                        }
+                    }
+
+                    @Override
+                    public void onCancelled(@NonNull DatabaseError error) {
+                        Toast.makeText(MyPageActivity.this, "데이터 로드 실패", Toast.LENGTH_SHORT).show();
+                    }
+                });
+            }
+        }
+    }
+
+    private void loadUserData() {
+        Intent intent = getIntent();
+        if (intent != null) {
+            String userId = intent.getStringExtra("UID");
+            if (userId != null) {
+                DatabaseReference userRef = FirebaseDatabase.getInstance().getReference("users").child(userId);
+                userRef.addListenerForSingleValueEvent(new ValueEventListener() {
+                    @Override
+                    public void onDataChange(@NonNull DataSnapshot snapshot) {
+                        if (snapshot.exists()) {
+                            String name = snapshot.child("name").getValue(String.class);
+                            if (name != null) {
+                                etNickName.setText(name);
+                            }
+                            String statusMsg = snapshot.child("statusMsg").getValue(String.class);
+                            if (statusMsg != null) {
+                                etStatusMsg.setText(statusMsg);
+                            }
+                        }
+                    }
+
+                    @Override
+                    public void onCancelled(@NonNull DatabaseError error) {
+                    }
+                });
+            }
+        }
     }
 
     private void selectImage() {
@@ -131,6 +223,7 @@ public class MyPageActivity extends AppCompatActivity {
         }
 
     }
+
     private void loadImages() {
         //로그인 후 정보를 인텐트로 넘기기 때문에 로그인 과정이 필수
         Intent intent = getIntent();
@@ -142,6 +235,7 @@ public class MyPageActivity extends AppCompatActivity {
             }
         }
     }
+
     private void loadImageFromFirebase(String imageType, String userId, ImageView imageView) {
         StorageReference storageReference = FirebaseStorage.getInstance()
                 .getReference("Mypage/images/" + imageType + "/" + userId);
@@ -151,11 +245,13 @@ public class MyPageActivity extends AppCompatActivity {
         storageReference.getDownloadUrl().addOnSuccessListener(uri -> {
             Glide.with(MyPageActivity.this)
                     .load(uri)
+                    .centerCrop()//이미지 꽉차게 설정
                     .into(imageView);
             ivBannerImg.setBackground(null);
         }).addOnFailureListener(exception -> {
             Glide.with(MyPageActivity.this)
                     .load(placeholderImage)
+                    .centerCrop()
                     .into(imageView);
         });
     }
@@ -166,14 +262,28 @@ public class MyPageActivity extends AppCompatActivity {
             @Override
             public void onComplete(@NonNull Task<UploadTask.TaskSnapshot> task) {
                 if (task.isSuccessful()) {
-                    Toast.makeText(MyPageActivity.this, imageType + " 이미지 업로드에 성공했습니다", Toast.LENGTH_SHORT).show();
+                    // 이미지 업로드 성공 시 다운로드 URL 가져오기
+                    storageReference.getDownloadUrl().addOnSuccessListener(uri -> {
+                        // 다운로드 URL을 리얼타임 데이터베이스에 저장
+                        saveImageUrlToDatabase(uri.toString(), imageType, Uid);
+                        Toast.makeText(MyPageActivity.this, imageType + " 이미지 업로드에 성공했습니다", Toast.LENGTH_SHORT).show();
+                    });
                 } else {
                     Toast.makeText(MyPageActivity.this, imageType + " 이미지 업로드에 실패했습니다", Toast.LENGTH_SHORT).show();
                 }
             }
         });
     }
-
+    private void saveImageUrlToDatabase(String imageUrl, String imageType, String Uid) {
+        DatabaseReference userRef = FirebaseDatabase.getInstance().getReference("users").child(Uid);
+        if (imageType.equals("profile")) {
+            userRef.child("profileImageUrl").setValue(imageUrl).addOnCompleteListener(task -> {
+            });
+        } else if (imageType.equals("banner")) {
+            userRef.child("bannerImageUrl").setValue(imageUrl).addOnCompleteListener(task -> {
+            });
+        }
+    }
     private void requestUpdateUserData() {
         // TODO: 사용자 데이터 업데이트 요청
     }
