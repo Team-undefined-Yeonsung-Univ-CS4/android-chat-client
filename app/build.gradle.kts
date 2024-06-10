621af0b3d52e81cb4a61d2179a74028ac0cda42e
DESKTOP-UV21EQH\zx221 <zx2210@naver.com>
2024-06-11 01:41:15 +0900

    gradle 추가

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -35,6 +35,7 @@ android {
 dependencies {
     implementation(platform("com.google.firebase:firebase-bom:32.3.1"))
     implementation("com.google.firebase:firebase-database")
+    implementation ("com.github.bumptech.glide:glide:4.12.0")
     implementation(libs.appcompat)
     implementation(libs.material)
     implementation(libs.activity)
